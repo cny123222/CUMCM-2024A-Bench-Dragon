1cbca6040277e7d240c397ba0edabe760e256705
cny123222 <cny123222@sjtu.edu.cn>
2025-06-27 20:13:15 +0800

    Delete redundant files

--- writes/write_result1.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-header = [[f'{second}s' for second in range(0, 301)]]
-header.insert(0, '')
-
-with open('my_result1.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(header)
